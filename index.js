e55411aa7905ae9d6efbcfb5c55e005e4632a2cd
piyawat sakdadet <piyawatbol@gmail.com>
2022-10-31 19:10:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const expres = require('express')
+const app = express()
+const PORT = 4000
+
+app.listen(PORT,()=> {
+    console.log(`Api Listening on PORT ${PORT}`)
+})
+
+
+app.get('/',(req ,res) =>{
+    res.send("this is my api running......")
+} )
+
+app.get('/about',(req,res)=>{
+    res.send("this is my about rout")
+
+})
+
+
+module.exports = app
